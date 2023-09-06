22f86008f9c84f4c6ce9f32f26361644ded4645d
mieshiii <mieseliel.delacruz@gmail.com>
2023-09-06 16:37:59 +0800

    #18 - conditionals on variables

--- main.tf
+++ main.tf
@@ -88,12 +88,12 @@ resource "aws_instance" "dev_node" {
   }
 
   provisioner "local-exec" {
-    command = templatefile("linux-ssh-config.tpl", {
+    command = templatefile("${var.host_os}-ssh-config.tpl", {
       hostname = self.public_ip,
       user = "ubuntu",
       identityfile = "~/.ssh/mtckey"
     })
     
-    interpreter = ["bash", "-c"]
+    interpreter = var.host_os == "linux" ? ["bash", "-c"] : ["Powershell", "-Command"]
   }
 }
